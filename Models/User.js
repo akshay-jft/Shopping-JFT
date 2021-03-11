0813665469734073e849269255f220ea9f40efc2
akshay_2304 <akshay.bhargava@jellyfishtechnologies.com>
2021-03-11 19:12:33 +0530

    BCrypt

--- Models/User.js
+++ Models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcryt = require('bcrypt')
+
 const UserSchema = new mongoose.Schema({
     email : {
         type : String,
@@ -13,6 +15,12 @@ const UserSchema = new mongoose.Schema({
         minlength : [6, 'Minimum Password length is 6 character'],
         required : [true, 'Please Enter a password']
     }
+})
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcryt.genSalt()
+    this.password = await bcryt.hash(this.password, salt)
+    next()
 })
 
 module.exports = mongoose.model('user', UserSchema)
